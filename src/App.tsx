512e028595b08880c51db7aba8c879dcd7d24dd4
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2022-02-15 16:55:28 +0300

    Demo updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {N1ED} from '@edsdk/n1ed-react';
-import {Editor} from '@tinymce/tinymce-react';
+import {Editor} from 'tinymce';
 
 class App extends React.Component {
 
@@ -13,6 +13,18 @@ class App extends React.Component {
             apiKey="REACDFLT"
             initialValue="<p>N1ED react demo</p>"
             onEditorChange={this.handleEditorChange}
+
+            init={{
+                height: 500,
+                toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | myCustomToolbarButton',
+                setup: (editor: any) => {
+                    console.log("TinyMCE initialized");
+                    editor.ui.registry.addButton('myCustomToolbarButton', {
+                        text: 'My Custom Button',
+                        onAction: () => window.alert('Custom button clicked!'),
+                    });
+                },
+            }}
         />;
     }
 }
